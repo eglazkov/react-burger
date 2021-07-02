3bc05d5cfdec19756b3a6b6cb91c97c51ccf8268
eglazkov <egorglazkov@mail.ru>
2021-07-03 00:32:30 +0300

    исправление замечаний

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,5 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect} from 'react'; 
+import ReactDOM from 'react-dom';
 import modalStyles from './modal.module.css';
 import {CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import ModalOverlay from '../modal-overlay/modal-overlay';
@@ -7,6 +8,8 @@ interface IModal {
   caption?: string,
   onClose: () => void
 };
+
+const modalRoot = document.getElementById("modals") as HTMLDivElement;
 
 const Modal: FC<IModal> = ({caption, children, onClose}) => {
   useEffect(() => {
@@ -19,30 +22,32 @@ const Modal: FC<IModal> = ({caption, children, onClose}) => {
   return () => window.removeEventListener('keydown', close)
   },[onClose])
 
-  return (
-    <>
-      <div
-        onClick={function(e) {
-          e.stopPropagation();
-        }}
-        className={modalStyles.container}>
-        <div className={`pl-10 pt-10 pr-10 pb-10 ${modalStyles.modal}`}>
-          <div className={modalStyles.modalHeader}>
-            <span className="text text_type_main-large">{caption}</span>
-            <button className={modalStyles.closeButton} onClick={function(e) {
-              e.stopPropagation();
-              onClose && onClose();
-            }}>
-              <CloseIcon type="primary"/>
-            </button>          
-          </div>
-          <div className={modalStyles.modalContent}>
-            {children}
+  return ReactDOM.createPortal(
+    (
+      <>
+        <div
+          onClick={function(e) {
+            e.stopPropagation();
+          }}
+          className={modalStyles.container}>
+          <div className={`pl-10 pt-10 pr-10 pb-10 ${modalStyles.modal}`}>
+            <div className={modalStyles.modalHeader}>
+              <span className="text text_type_main-large">{caption}</span>
+              <button className={modalStyles.closeButton} onClick={function(e) {
+                e.stopPropagation();
+                onClose && onClose();
+              }}>
+                <CloseIcon type="primary"/>
+              </button>          
+            </div>
+            <div className={modalStyles.modalContent}>
+              {children}
+            </div>
           </div>
         </div>
-      </div>
-    <ModalOverlay onClick={onClose}/>
-    </>
+        <ModalOverlay onClick={onClose}/>
+      </>
+    ), modalRoot
   );
 }
  
